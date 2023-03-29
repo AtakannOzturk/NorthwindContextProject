53a17cfc12daa6a49c28e3daf2a68b40067df9a2
AtakannOzturk <125278779+AtakannOzturk@users.noreply.github.com>
2023-03-29 16:12:25 +0300

    29.03.2023

---
+++ Entities/DTOs/ProductDetailDto.cs
@@ -0,0 +1,20 @@
+﻿using Core;
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs
+{
+    //Data Trasnformation Object
+    //Taşınacak oBjeler
+    public class ProductDetailDto:IDto
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string CategoryName { get; set; }
+        public short UnitsInStock { get; set; }
+    }
+}
