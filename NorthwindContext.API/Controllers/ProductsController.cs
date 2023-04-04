aa07569449c796b5ecf4cae36f7cff077ca6b98e
AtakannOzturk <125278779+AtakannOzturk@users.noreply.github.com>
2023-04-04 17:51:44 +0300

    4.04.2023

---
+++ NorthwindContext.API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NorthwindContext.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled
+        //IoC Container --- Inversion  of control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get() 
+        {
+            
+            var result=_productService.GetAll();
+            return result.Data;
+        }
+    }
+}
